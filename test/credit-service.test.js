f06bb6d4682e75c3a89aaef07c6dff9fea1229f3
Iago <iagoaroucaferreira@hotmail.com>
2026-08-19 15:39:56 -0300

    test(credit): improve test setup and execution

--- test/credit-service.test.js
+++ test/credit-service.test.js
@@ -13,37 +13,34 @@ describe('Credit Service - Suíte de Testes', () => {
     const testEnv = cds.test(__dirname + '/..');
 
     beforeEach(async () => {
-        const { Customers, LimitChangeRequests } = cds.entities('CreditService');
+    const { Customers, LimitChangeRequests } =
+        cds.entities('CreditService');
 
-        await cds.run(DELETE.from(LimitChangeRequests));
-        await cds.run(DELETE.from(Customers));
+    await cds.run(DELETE.from(LimitChangeRequests));
+    await cds.run(DELETE.from(Customers));
 
-        await cds.run(
-            INSERT.into(Customers).entries({
+    await cds.run(
+        INSERT.into(Customers).entries([
+            {
                 ID: 'CUST-1001',
                 name: 'Empresa Alpha Ltda',
                 currentLimit: 50000.00,
                 usedCredit: 12000.00
-            })
-        );
-
-        await cds.run(
-            INSERT.into(Customers).entries({
+            },
+            {
                 ID: 'CUST-1003',
                 name: 'Comércio de Bebidas Silva',
                 currentLimit: 25000.00,
                 usedCredit: 22000.00
-            })
-        );
-
-        await cds.run(
-            INSERT.into(Customers).entries({
+            },
+            {
                 ID: 'CUST-1002',
                 name: 'Tech Solutions S/A',
                 usedCredit: 25000.00
-            })
-        );
-    })
+            }
+        ])
+    );
+});
 
     test('Deve auto-aprovar solicitação com aumento de até 20%', async () => {
         const { Customers } = cds.entities('CreditService')
